46d03f76aa249a43b5eb0944eea4a383dffa5a9a
Maximus <79494898+Maximus-Raafat@users.noreply.github.com>
2024-03-29 16:07:00 +0200

    edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { InterFaceComponent } from './interFace/inter-face/inter-face.component'
 
 export const routes: Routes = [
     { path: '', redirectTo: '/sartApp', pathMatch: 'full' },
-    { path: 'sartApp', component: InterFaceComponent },
-    { path: 'qieze', component: QuizeCompComponent },
-    { path: 'result', component: ResultComponent },
+    { path: '/sartApp', component: InterFaceComponent },
+    { path: '/qieze', component: QuizeCompComponent },
+    { path: '/result', component: ResultComponent },
 
 ];
