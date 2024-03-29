1fae05fea291cd7410e3fc8667cc638b74de633e
Maximus <79494898+Maximus-Raafat@users.noreply.github.com>
2024-03-29 16:11:39 +0200

    some change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { QuizeCompComponent } from './quizeComp/quize-comp/quize-comp.component'
 import { InterFaceComponent } from './interFace/inter-face/inter-face.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/sartApp', pathMatch: 'full' },
-    { path: '/sartApp', component: InterFaceComponent },
-    { path: '/qieze', component: QuizeCompComponent },
-    { path: '/result', component: ResultComponent },
+    { path: '', redirectTo: 'sartApp', pathMatch: 'full' },
+    { path: 'sartApp', component: InterFaceComponent },
+    { path: 'qieze', component: QuizeCompComponent },
+    { path: 'result', component: ResultComponent },
 
 ];
